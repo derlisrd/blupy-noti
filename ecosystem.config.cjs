0e49bb8b4cb7071d801553f69ed2901344e7783a
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-03-12 17:26:01 -0300

    dotenv

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,12 +1,11 @@
 module.exports = {
-    apps : [
-        {
-          name: "push",
-          script: "./dist/serve.js",
-          watch: true,
-          env: {
-              "NODE_ENV": "production"
-          },
-        }
-    ]
-  }
+  apps: [{
+    name: "push",
+    script: "./dist/serve.js",
+    interpreter: "node",
+    interpreter_args: "--env-file=.env",
+    env: {
+      NODE_ENV: "production"
+    }
+  }]
+};
