b07ee0a0d18e902a934d6ef221c31ad3d5311922
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-09-11 18:43:22 -0300

    defalut

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,6 @@ module.exports = {
     name: "push",
     script: "./dist/serve.js",
     interpreter: "node",
-    instances: 2, 
     interpreter_args: "--env-file=.env",
     env: {
       NODE_ENV: "production"
