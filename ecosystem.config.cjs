b848417388a3d4b8a9a43367b5e8b7a851a9f249
derlis-micredito <derlis.ruizdiaz@micredito.com.py>
2025-09-11 18:41:26 -0300

    instancias

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,9 +3,11 @@ module.exports = {
     name: "push",
     script: "./dist/serve.js",
     interpreter: "node",
+    instances: 2, 
     interpreter_args: "--env-file=.env",
     env: {
       NODE_ENV: "production"
-    }
+    },
+    time: true
   }]
 };
